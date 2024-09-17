51c067e3295725447a22a73a3bc2b7e1771c2e4a
vala-asgeirs <vala.asgeirsdottir@gmail.com>
2024-09-17 16:35:21 +0200

    adding statistics script

---
+++ summary_statistics.py
@@ -0,0 +1,90 @@
+import numpy as np
+import pandas as pd
+
+# Open the file containing our data
+df = pd.read_csv("data/penguins_size.csv")
+
+# Replace . that are in our data with NaN
+df.replace('.', np.nan, inplace=True)
+
+# Drop all rows that contain NaN as a value, since we don't want to use those in our calculations
+df = df.dropna()
+
+# The numerical attributes we want to use are in columns 3, 4, 5 and 6
+cols = range(2, 6)
+
+# Get the names of the attributes we want to use
+numAttributeNames = np.asarray(df.columns[cols])
+
+# Drop all columns exept the one we want to use from our dataframe
+num_df = df.iloc[:, cols]
+
+# Create a dictionary where each attribute name will be a key
+num_attribute_dict = dict()
+
+# Create another dictionary where each attribute name will be a key
+perc_attribute_dict = dict()
+
+covarience = num_df.cov()
+print(covarience)
+
+correlation = num_df.corr()
+print(correlation)
+
+for attr in numAttributeNames:
+    # Create a dictionary where the name of each statistic will be a key
+    statistics_dict = dict()
+    statistics_dict["Mean"] = num_df[attr].mean()
+    statistics_dict["Varience"] = num_df[attr].var()
+    statistics_dict["Standard Deviation"] = num_df[attr].std()
+
+    # Create another dicitonary for the percintiles, where each statistic will be a key
+    percintile_dict = dict()
+    percintile_dict["Min"] = num_df[attr].min()
+    percintile_dict["25% Quantile"] = num_df[attr].quantile(q=0.25)
+    percintile_dict["Median"] = num_df[attr].median()
+    percintile_dict["75% Quantile"] = num_df[attr].quantile(q=0.75)
+    percintile_dict["Max"] = num_df[attr].max()
+    percintile_dict["Range"] = num_df[attr].max() - num_df[attr].min()
+    
+    num_attribute_dict[attr] = statistics_dict
+    perc_attribute_dict[attr] = percintile_dict
+
+# Convert our dictionary of dictionaries to a Pandas dataframe that contains our statistics summary
+statistics_df = pd.DataFrame(data=num_attribute_dict)
+
+print(statistics_df)
+print()
+
+percentile_df = pd.DataFrame(data=perc_attribute_dict)
+
+print(percentile_df)
+print()
+
+# Get the column that contains species in our data
+species = df["species"]
+
+# Count how many times each species appears in the data
+speciesCount = species.value_counts()
+
+print(speciesCount)
+print()
+
+# Get the column that contains islands in our data
+islands = df["island"]
+
+# Count how many times each island appears in the data
+islandCount = islands.value_counts()
+
+print(islandCount)
+print()
+
+# Get the column that contains sexes in our data
+sex = df["sex"]
+
+# Count how many times each value of sex appears
+sexCount = sex.value_counts()
+
+print(sexCount)
+print()
+
